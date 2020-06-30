38e86c34b8908efa64b0570a0006e7332ddd47ea
Varya Deviaterikova <hallovarvara@gmail.com>
2020-06-30 16:37:15 +0300

    refactor: solve merge conflicts

---
+++ rslang/src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import MainPage from '../pages/Main';
+import SettingsPage from '../pages/Settings';
+import StatisticPage from '../pages/Statistic';
+import TeamPage from '../pages/Team';
+import PromoPage from '../pages/Promo';
+import VocabularyPage from '../pages/Vocabulary';
+
+const Main = () => (
+  <main className="main">
+    <Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/settings" component={SettingsPage} />
+      <Route path="/statistic" component={StatisticPage} />
+      <Route path="/team" component={TeamPage} />
+      <Route path="/promo" component={PromoPage} />
+      <Route path="/vocabulary" component={VocabularyPage} />
+    </Switch>
+  </main>
+);
+
+export default Main;
