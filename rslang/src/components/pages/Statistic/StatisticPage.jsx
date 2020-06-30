77e3b69ffb0c5602ef01ff51ece6f00455f0b29f
Varya Deviaterikova <hallovarvara@gmail.com>
2020-06-30 12:03:51 +0300

    fix: change git config - core.ignorecase false

---
+++ rslang/src/components/pages/Statistic/StatisticPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StatisticPage = () => (
+  <div>
+    <h2 className="title">Statistic</h2>
+    <ul>
+      <li className="text">1</li>
+      <li className="text">2</li>
+      <li className="text">3</li>
+    </ul>
+  </div>
+);
+export default StatisticPage;
