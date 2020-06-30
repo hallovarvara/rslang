c9f0c31a944463f2114ac85ca156293230b416fd
Varya Deviaterikova <hallovarvara@gmail.com>
2020-06-30 11:52:55 +0300

    fix: change name of general Components` folder and delete unused vars

--- rslang/src/basicComponents/Select/SelectView.jsx
+++ rslang/src/basicComponents/Select/SelectView.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Select, MenuItem } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
 
 const mapSelectTitlesToItems = (title, index) => (
     <MenuItem key={index} value={title} classes={{ root: 'menu-item' }}>{title}</MenuItem>
