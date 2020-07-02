9ac5dd91e74c5079d5351df0d2056ecbf5ae92c0
Dmitry Lebetsky <dmitriy.lebetski@mail.ru>
2020-07-02 23:47:45 +0300

    feat: add dropdown list with the list of games

--- rslang/src/components/Header/HeaderView.jsx
+++ rslang/src/components/Header/HeaderView.jsx
@@ -3,11 +3,28 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
+import MenuList from '../../basicComponents/MenuList';
+
+import { gamesData } from '../../helpers/constants';
+
 const mapLinkTitlesToItems = (linkTitle, index) => {
   const linkPath = linkTitle.toLowerCase().split(' ').join('-');
   return (
     <li key={index} className="navigation__item">
-      <NavLink activeClassName="navigation__item_active" to={`/${linkPath}`}>{linkTitle}</NavLink>
+      {
+        linkTitle === 'Play games'
+          ? <MenuList
+              menuTitle={<NavLink activeClassName="navigation__item_active" to={`/${linkPath}`}>{linkTitle}</NavLink>}
+              menuItems={gamesData.map((gameObj, i) => (
+                <NavLink
+                  className="menu-list-item__link"
+                  activeClassName="navigation__item_active"
+                  key={i}
+                  to={gameObj.link}>{gameObj.title}</NavLink>
+              ))}
+            />
+          : <NavLink activeClassName="navigation__item_active" to={`/${linkPath}`}>{linkTitle}</NavLink>
+      }
     </li>
   );
 };
@@ -21,7 +38,7 @@ const HeaderView = ({ linkTitles, isUserLogged }) => (
               linkTitles.map(mapLinkTitlesToItems)
             }
             {
-              isUserLogged && <li className="navigation__item">
+              isUserLogged && <li className="navigation__item exit-icon">
                 <NavLink activeClassName="navigation__item_active" to="sign-up">
                   <ExitToAppIcon color="disabled" style={{ fontSize: '3rem' }}/>
                 </NavLink>
