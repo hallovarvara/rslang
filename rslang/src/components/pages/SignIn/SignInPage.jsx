f27612f64b88075c4c5737988f4a22983a6a4b89
Artem Rimarchyk <1patrik@mail.ru>
2020-07-09 01:46:29 +0300

    fix: delete is_js

--- rslang/src/components/pages/SignIn/SignInPage.jsx
+++ rslang/src/components/pages/SignIn/SignInPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import is from 'is_js';
 import { connect } from 'react-redux';
 import Input from '../../../basicComponents/Input';
 import Button from '../../../basicComponents/Button';
@@ -12,9 +11,14 @@ const SignInPage = (props) => {
   const [password, setPass] = React.useState('');
   const [emailValid, setEmailValid] = React.useState(true);
 
+  const isEmailValid = (mail) => {
+    const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+    return setEmailValid(reg.test(String(mail)));
+  };
+
   const onChangeEmail = (e) => {
-    setEmailValid(is.email(e.target.value) && e.target.value);
     setEmail(e.target.value);
+    isEmailValid(e.target.value);
   };
   const onChangePass = (e) => {
     setPass(e.target.value);
--- rslang/src/components/pages/SignUp/SignUpPage.jsx
+++ rslang/src/components/pages/SignUp/SignUpPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import is from 'is_js';
 import Input from '../../../basicComponents/Input';
 import Button from '../../../basicComponents/Button';
 import UserService from '../../../helpers/UserService';
@@ -27,14 +26,19 @@ const SignUpPage = () => {
     return setNickValid(reg.test(String(nick)));
   };
 
+  const isEmailValid = (mail) => {
+    const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+    return setEmailValid(reg.test(String(mail)));
+  };
+
   const isFormValid = () => {
     return emailValid && passValid && passRepeatValid && nickValid;
 
   };
 
   const onChangeEmail = (e) => {
-    setEmailValid(is.email(e.target.value) && e.target.value);
     setEmail(e.target.value);
+    isEmailValid(e.target.value);
   };
   const onChangeNick = (e) => {
     setNick(e.target.value);
