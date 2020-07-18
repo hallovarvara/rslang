33f705fad4e870faf32bd8316c7c285d423bcaf1
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-07-19 00:01:37 +0300

    feat: render statistic

--- rslang/src/components/AudioCallGame/components/StatisticPage/StatisticPageView.jsx
+++ rslang/src/components/AudioCallGame/components/StatisticPage/StatisticPageView.jsx
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import classNames from 'classnames';
 import { text } from '../../../../helpers/constants';
+import FinishGameItem from '../FinishGameItem';
 
 const StatisticPageView = ({ 
   statistic,
   handleShowStatistic,
   handleClickNewGame,
 }) => {
+  const generateItemsWords = (array) => (
+    array.length !== 0 && array.map((word) => (
+      <FinishGameItem
+        key={word.id}
+        word={word}
+      />
+    ))
+  );
   const { ru: { button: { newGame, currentResults } } } = text;
   const buttonStyle = classNames('button', 'button_big');
   return (
@@ -16,8 +25,24 @@ const StatisticPageView = ({
     {  JSON.parse(statistic).map((day, index) => {
       return (
         <div key={index} className="statistic__item">
-          tttkkvkbkbkkb
+        <h2 className="statistic__date">{day.date}</h2>
+        <div className="statistic__item_result">
+          <div>
+            <h2 className="statistic__item_title">
+              { text.ru.answersCorrect }
+              <span className="right">{day.right.length}</span>
+            </h2>
+            {generateItemsWords(day.right)}
+          </div>
+          <div>
+            <h2 className="statistic__item_title">
+            { text.ru.answersMistaken }
+              <span className="wrong">{day.error.length}</span>
+            </h2>
+            {generateItemsWords(day.error)}
+          </div>
         </div>
+      </div>
       )})
     }
     <div className="button__container">
